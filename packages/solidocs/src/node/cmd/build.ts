9713140b7b7512f098d19f09abd541ca0a6cff40
-akku- <151517253+akku1139@users.noreply.github.com>
2025-07-07 05:16:18 +0000

    fix: basepath

--- packages/solidocs/src/node/cmd/build.ts
+++ packages/solidocs/src/node/cmd/build.ts
@@ -16,6 +16,8 @@ export const cmd: CMD<typeof argsSchema> = async (config, _args) => {
   process.env.NODE_ENV = "production"
   // const cwd = process.cwd()
 
+  const clientBaseDir = "/_assets/js"
+
   const distDir = p(".solidocs/dist")
   await fs.rm(distDir, { recursive: true, force: true })
   await fs.mkdir(distDir, { recursive: true })
@@ -29,7 +31,7 @@ export const cmd: CMD<typeof argsSchema> = async (config, _args) => {
   const clientBuildResult = await rolldownBuild({
     input: path.resolve(import.meta.dirname, "../../client/entry/client.tsx"),
     output: {
-      dir: p(distDir + "/_assets/js"),
+      dir: p(distDir + clientBaseDir),
       format: "esm",
     },
     platform: "browser",
@@ -83,7 +85,7 @@ export const cmd: CMD<typeof argsSchema> = async (config, _args) => {
 
   await Promise.all(routes.map(async route => {
     console.log("path:", route[0])
-    const content = "<!DOCTYPE html>"+await render(route[0], "/_assets/js/" + clientBuildResult.output[0].fileName, config.basePath)
+    const content = "<!DOCTYPE html>"+await render(route[0], config.basePath.replace(/\/+$/, "") + clientBaseDir + clientBuildResult.output[0].fileName, config.basePath)
     await fs.writeFile(path.resolve(distDir, route[1].replace(/.md$/, ".html")), content)
   }))
 
